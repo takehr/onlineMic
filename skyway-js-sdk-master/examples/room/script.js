8777efffbc5dea2b5bc9ecf24c6d04e84fa93e75
takehr <takehiro.cit@gmail.com>
2020-12-06 00:07:30 +0900

    change to sound only

--- skyway-js-sdk-master/examples/room/script.js
+++ skyway-js-sdk-master/examples/room/script.js
@@ -29,7 +29,7 @@ const Peer = window.Peer;
   const localStream = await navigator.mediaDevices
     .getUserMedia({
       audio: true,
-      video: true,
+      video: false,
     })
     .catch(console.error);
 
