14f7918f9bba5bd921a97fd06b42d60b655db70a
takehr <takehiro.cit@gmail.com>
2020-12-07 21:13:03 +0900

    test

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ document.querySelector('#find-me').addEventListener('click', geoFindMe);
     }
     window.roomId=roomId.value;
 
-    const room = peer.joinRoom(rommId.value, {
+    const room = peer.joinRoom(peer.id, {
       mode: getRoomModeByHash(),
       stream: localStream,
     });
