95ea4626956033a8395c82fbd4f619a8e0f231e9
takehr <takehiro.cit@gmail.com>
2020-12-08 15:03:01 +0900

    test

--- updateClient/script.js
+++ updateClient/script.js
@@ -26,7 +26,7 @@ peer.once('open',() =>{
                 activePeers = peers;
             });
         database.ref('/peers').once('value').then((snapshot) => {
-            alert(JSON.stringify(snapshot));
+            snapshot.foreach(el => console.log(el););
 //            for key in snapshot.key():
 //                for activePeer in activePeers:
 //                    if(key!==activePeer){
