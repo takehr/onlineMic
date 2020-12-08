b1aa8eee32ae8b66b707d73272996b293e716b84
takehr <takehiro.cit@gmail.com>
2020-12-08 14:14:32 +0900

    same previous

--- updateClient/script.js
+++ updateClient/script.js
@@ -26,7 +26,7 @@ peer.once('open',() =>{
                 activePeers = peers;
             });
         database.ref('/peers').once('value').then((snapshot) => {
-            alert(snapshot.toSource());
+            alert(JSON.stringify(snapshot));
             });
         },10000);
 });
